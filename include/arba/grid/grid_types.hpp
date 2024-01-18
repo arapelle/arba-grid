a0dd82fee46e228ad80900d0d18a1a465a404509
Aymeric Pellé <aympelle@gmail.com>
2024-01-18 17:07:51 +0100

    v0.4.0:11: Add types grid_position8,  grid_position16,  grid_position32,  grid_position64.
    grid_position is still grid_position32.

--- include/arba/grid/grid_types.hpp
+++ include/arba/grid/grid_types.hpp
@@ -7,9 +7,15 @@ inline namespace arba
 namespace grid
 {
 
-using grid_position = math::vec2i16;
-using grid_offset = math::vec2i16;
-using grid_dimension = math::vec2u16;
+using grid_position8 = math::vec2i8;
+using grid_position16 = math::vec2i16;
+using grid_position32 = math::vec2i32;
+using grid_position64 = math::vec2i64;
+
+using grid_position = grid_position32;
+
+using grid_offset = math::vec2i32;
+using grid_dimension = math::vec2u32;
 
 }
 }
